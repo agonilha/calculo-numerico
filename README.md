880b965ec83b479e0c6f23d776cad2b6e5955040
Henrique Cavalieri <henrique.cavalieri@leaodiesel.com.br>
2023-03-10 14:04:48 -0300

    Create basic Java template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# calculo-numerico
+# Cálculo Numérico
+Este repositório é usado para o desenvolvimento e armazenamento dos exercícios e trabalhos de cálculo numérico da Unifil no ano de 2023.
---
+++ src/Floating_Point/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    int mantissa = 0;
+    float x1 = 0.0f;
+    float x2 = 0.0f;
+    float resultado = 0.0f;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
