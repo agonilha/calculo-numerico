c88643fa8ec4a4aca4594415269a1f0ed7aeedef
agonilha <henriqueagonilha@gmail.com>
2023-03-13 17:07:21 -0300

    Started thinking process

--- src/Floating_Point/Main.java
+++ src/Floating_Point/Main.java
@@ -4,6 +4,15 @@ public class Main {
     float x2 = 0.0f;
     float resultado = 0.0f;
 
+    public float[] toFloating(float number, int mantissa){
+        // Calculate how many digits
+
+        // exemplo: 5420.5 seria [0.5420, 10, 4] se a mantissa for 4
+        float[] array = new float[3];
+
+
+        return array;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
